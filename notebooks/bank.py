0ab35dc5bed7dab00a888a3c12f34afa8e89b78d
gubli <tisina@tuta.io>
2025-05-18 12:29:45 +0000

    feat: add data renaming and type conversion functionality in bank.py

--- notebooks/bank.py
+++ notebooks/bank.py
@@ -20,3 +20,25 @@ import joblib
 data=pd.read_csv('/home/tisinr/MEGA/Dev/models/classifier/dataset/bank.csv',header=0, sep=';')
 # Display the first few rows of the dataset
 print(data.head())
+
+def rename_n_change(data):
+    # Rename columns for better readability
+    data.rename(columns={
+        'marital':'marital_status',
+        'default':'credit_default',
+        'housing':'housing_loan',
+        'loan':'personal_loan',
+        'y':'target'}, inplace=True)
+    #change data types
+    data['target'] = data['target'].astype('category')
+    data['marital_status'] = data['marital_status'].astype('category')
+    data['education'] = data['education'].astype('category')
+    data['job'] = data['job'].astype('category')
+    data['contact'] = data['contact'].astype('category')
+    data['month'] = data['month'].astype('category')
+    data['day'] = data['day'].astype('category')
+    data['credit_default'] = data['credit_default'].astype('category')
+    data['housing_loan'] = data['housing_loan'].astype('category')
+    data['personal_loan'] = data['personal_loan'].astype('category')
+    return data
+data=rename_n_change(data)
